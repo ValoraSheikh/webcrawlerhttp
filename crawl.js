a5641839990dec3fd8715f48a45e0e73018b7114
Aman Sheikh <haarishsheikh04@gmail.com>
2024-11-07 20:15:15 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,42 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === `/`) {
+            // Relative
+            try {
+                let urlObj = new URL (`${baseURL}${linkElement.href}`);
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with relative url:${error.message}`);
+            }
+        } else{
+            // Absolute
+            try {
+                let urlObj = new URL (linkElement.href);
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with relative url:${error.message}`);
+            }
+        }
+    }
+    return urls;
+}
+
 function normalizeURL(urlString) {
-    let urlObj = new URL(urlString)
-    let hostPath = `${urlObj.hostname}${urlObj.pathname}`
+    let urlObj = new URL(urlString);
+    let hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     if (hostPath.length > 0 && hostPath.slice(-1) === `/`) { 
         return hostPath.slice(0, -1) // for removing `/` in URL
     }
-    return hostPath
+    return hostPath;
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,31 +1,104 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
+// Test for `normalize URL` function
+
 test('normalizeURL strip protocol', () => {
-    const input = 'https://blog.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
 })
 
 
 test('normalizeURL strip trailing slash ', () => {
-    const input = 'https://blog.boot.dev/path/' // Adding a `/` at end of URL, if user enter it they should get to where they want
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+    const input = 'https://blog.boot.dev/path/'; // Adding a `/` at end of URL, if user enter it they should get to where they want
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
 })
 
 test('normalizeURL capitals', () => {
-    const input = 'https://BLOG.boot.dev/path' // It works correctly because of URL is case sensetive `using new URL in crawl.js`
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+    const input = 'https://BLOG.boot.dev/path'; // It works correctly because of URL is case sensetive `using new URL in crawl.js`
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
 })
 
 test('normalizeURL strip http', () => {
-    const input = 'http://blog.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+    const input = 'http://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+// Test for `getURLsFromHTML` function
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev"> 
+            Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+    // JSDOM put trailing slash at HTML URL
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+    const expected = ["https://blog.boot.dev/"]; // So output is look like this 
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="/path/"> 
+            Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `; // This is relative URL so you need to add trailing slash
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"]; // So output is look like this 
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/"> 
+            Boot.dev Blog path 1
+            </a>
+            <a href="/path2/"> 
+            Boot.dev Blog path 2
+            </a>
+        </body>
+    </html>
+    `;
+    // JSDOM put trailing slash at HTML URL
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/" ]; // So output is look like this 
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="invalid"> 
+            Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+    const expected = []; // So output is look like this 
+    expect(actual).toEqual(expected);
 })
